1f8d3f7aa3047db2d95da43822310a5a3747d7d6
Joe Curtin <42@jbcurtin.io>
2020-09-04 05:11:55 -0400

    env setup

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 apt-get update
-apt-get install python-virtualenv curl build-essential gcc-4.8 -y
+apt-get install -y python-virtualenv curl build-essential gcc-4.8
+apt-get install -y python-virtualenv
 pip install -e git+https://github.com/eteq/nbpages.git@b9ec8410803357939210e068af7e14a6f0625fab#egg=nbpages
-pip install -U pip jupyterlab python-virtualenv
+pip install -U pip jupyterlab
 
 # conda info --envs
 # conda env update --file=environment.yml
